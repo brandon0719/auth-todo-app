86133d3e68925c9c8bce3135d08ff845dc757ce0
Brandon Wong <brandon.r.wong@stonybrook.edu>
2024-08-20 21:21:04 -0400

    readme

--- server/db.js
+++ server/db.js
@@ -1,8 +1,10 @@
 const Pool = require("pg").Pool;
+const dotenv = require("dotenv");
+dotenv.config();
 
 const pool = new Pool({
     user: "postgres",
-    password: "Jhs19411360",
+    password: process.env.DATABASE_PW,
     host: "localhost",
     port: 5432,
     database: "authtodolist",
